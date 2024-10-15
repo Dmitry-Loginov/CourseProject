756c73388c940af89754f0c3d3a24144225bc60f
Dmitry Loginov <interestingfacts223@gmail.com>
2024-10-15 17:09:28 +0300

    .

--- Model.Context.cs
+++ Model.Context.cs
@@ -19,7 +19,55 @@ namespace Log
             : base("name=LogEntities")
         {
         }
-    
+
+        private static LogEntities _instance;
+
+        private static readonly object _lock = new object();
+
+        public static LogEntities GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new LogEntities();
+                        IsExistInstance = true;
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        public static void DeleteInstance()
+        {
+            if (_instance != null)
+            {
+                lock (_lock)
+                {
+                    if (_instance != null)
+                    {
+                        _instance.Dispose();
+                        IsExistInstance = false;
+                    }
+                }
+            }
+        }
+
+        public new int SaveChanges()
+        {
+            int countObject = 0;
+            //try
+            countObject = base.SaveChanges();
+            //catch { }
+            _instance = null;
+            return countObject;
+        }
+
+        public static bool IsExistInstance { get; set; }
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             throw new UnintentionalCodeFirstException();
